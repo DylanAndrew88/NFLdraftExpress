ed502a81234c398efe05d20480ac78e8dd34cde8
DylanAndrew88 <Dandrewblake88@gmail.com>
2017-07-26 00:24:04 -0400

    basic styling for readability purposes

--- app.js
+++ app.js
@@ -16,16 +16,16 @@
 
   //Routes
   app.get('/', function(req, res) {
-    res.render('index')
+    res.render('index');
   });
 
   app.get('/draft2017_round1', function(req, res) {
     models.nfl2017draftpicks.findAll({where: { roundnumber: 1}}).then(function(picks){
     res.render('draft2017_round1', {picks :picks})
-    })
+    });
   });
 
   //Listening
   app.listen(3005, function() {
-    console.log('Listening on Port 3005')
+    console.log('Listening on Port 3005');
   })
