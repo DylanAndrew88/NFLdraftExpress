9fc63e37d25023519a752e11a00fb2d81c616c66
DylanAndrew88 <Dandrewblake88@gmail.com>
2017-07-31 11:18:37 -0400

    order db item rendering in mustache by id

--- app.js
+++ app.js
@@ -21,13 +21,10 @@
   });
 
   app.get('/draft2017_round1', function(req, res) {
-    models.nfl2017draftpicks.findAll({where: {roundnumber: 1}}).then(function(picks){
+    models.nfl2017draftpicks.findAll({where: {roundnumber: 1}, order: [['id']]}).then(function(picks){
     res.render('draft2017_round1', {picks: picks});
     // for(i=0; i>models.nfl2017draftpicks.picknumberround
     // models.nfl2017draftpicks.findAll({where: {picknumberround: ()}}).then(console.log);
-    models.nfl2017draftpicks.findAll({where: {picknumberoverall: 1}}).then(function(topFive){
-    console.log(topFive);
-    });
     });
   });
 
